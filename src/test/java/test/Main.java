b26afa031f75cbfd1751e09aad1a59da0819b03b
Ronald <ronald_reir21@hotmail.com>
2018-12-04 22:09:58 -0600

    solo el main funciona

--- src/test/java/test/Main.java
+++ src/test/java/test/Main.java
@@ -14,44 +14,63 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+import org.junit.Assert;
 
 /**
  *
  * @author debianyisus
  */
 public class Main {
+
     /*
     url= https://www.browserstack.com
     email: ronald21122@gmail.com
     clave: dlik2194
-    */
-  public static final String USERNAME = "ronaldibanez1";
-  public static final String AUTOMATE_KEY = "rRsA7fWPys8P1AJcJuqs";
-  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+     */
+    public static final String USERNAME = "ronaldibanez1";
+    public static final String AUTOMATE_KEY = "rRsA7fWPys8P1AJcJuqs";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
     /**
      * @param args the command line arguments
      * @throws java.net.MalformedURLException
      */
     public static void main(String[] args) throws MalformedURLException {
-         
-    DesiredCapabilities caps = new DesiredCapabilities();
-    caps.setCapability("browserName", "iPhone");
-    caps.setCapability("device", "iPhone 8 Plus");
-    caps.setCapability("realMobile", "true");
-    caps.setCapability("os_version", "11.0");
+       // Iphone ip = new Iphone(USERNAME, AUTOMATE_KEY, URL);
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browserName", "iPhone");
+        caps.setCapability("device", "iPhone 8 Plus");
+        caps.setCapability("realMobile", "true");
+        caps.setCapability("os_version", "11.0");
 
-    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
-    driver.get("http://www.google.com");
-    WebElement element = driver.findElement(By.name("q"));
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver.get("https://av.occ.ues.edu.sv/");
+        
+        
+        driver.findElement(By.id("username")).sendKeys("CA16009");
+        pausa(500);
+        driver.findElement(By.id("password")).sendKeys("15669");
+        pausa(500);
+        driver.findElement(By.id("loginbtn")).submit();
+        ///Assert.assertEquals("Área personal", driver.getTitle());
+        pausa(500);
+        driver.findElement(By.linkText("Programacion III")).click();
+        pausa(10000);
+        
+                
+        
+/*
+        element.sendKeys("BrowserStack");
+        element.submit();*/
 
-    element.sendKeys("BrowserStack");
-    element.submit();
+        System.out.println(driver.getTitle());
+        driver.quit();
 
-    System.out.println(driver.getTitle());
-    driver.quit();
-    
-    
     }
-    
+
+    public static void pausa(long sleeptime){
+        try{
+            Thread.sleep(sleeptime);
+        } catch(InterruptedException ex){};
+    }
 }
